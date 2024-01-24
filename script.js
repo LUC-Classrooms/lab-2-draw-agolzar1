47871a2bc515710e2ba0078f1125832255dfe8d4
agolzar1 <156872398+agolzar1@users.noreply.github.com>
2024-01-24 12:20:50 -0600

    Update script.js

--- script.js
+++ script.js
@@ -4,9 +4,20 @@ function setup(){
 
 function draw(){
   background(200);
+  fill(255,0,0);
+  strokeWeight(2);
   ellipse(350,350,80,80);
-  line(50,40,20,20);
+  strokeWeight(7);
+  line(200,40,20,20);
+  strokeWeight(20);
+  strokeJoin(ROUND);
+  fill(0,376,0);
   rect(50,60,95,70);
+  strokeWeight(10);
+  fill(0,0,872);
   triangle(200,57,292,31,370,90);
+  noFill();
+  strokeWeight(13);
+  arc(200,180,150,150,0, HALF_PI);
   
 }
